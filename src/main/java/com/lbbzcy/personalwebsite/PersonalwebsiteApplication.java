75e60be1d07d06463fbdc702feac6dc98dc23809
zhuchunyu <1109927601@qq.com>
2017-10-25 22:22:24 +0800

    springboot+gradle项目搭建

---
+++ src/main/java/com/lbbzcy/personalwebsite/PersonalwebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.lbbzcy.personalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalwebsiteApplication.class, args);
+	}
+}
